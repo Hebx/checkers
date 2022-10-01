c60a2aa001c597c074d826d29edfa3033682f4d2
Developer Experience team at Tendermint <hello@tendermint.com>
2022-10-01 08:30:28 +0000

    Initialized with Ignite CLI

---
+++ x/checkers/genesis_test.go
@@ -0,0 +1,29 @@
+package checkers_test
+
+import (
+	"testing"
+
+	keepertest "github.com/hebx/checkers/testutil/keeper"
+	"github.com/hebx/checkers/testutil/nullify"
+	"github.com/hebx/checkers/x/checkers"
+	"github.com/hebx/checkers/x/checkers/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.CheckersKeeper(t)
+	checkers.InitGenesis(ctx, *k, genesisState)
+	got := checkers.ExportGenesis(ctx, *k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}
---
+++ x/checkers/keeper/grpc_query.go
@@ -0,0 +1,7 @@
+package keeper
+
+import (
+	"github.com/hebx/checkers/x/checkers/types"
+)
+
+var _ types.QueryServer = Keeper{}
